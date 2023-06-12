e6249bee8a59f167f4a5cb7e71842759e3d60187
TomFardell <50525775+TomFardell@users.noreply.github.com>
2023-06-12 15:31:58 +0100

    Connected Bank and Account class. Bank class stores accounts in a file

--- Account.java
+++ Account.java
@@ -8,16 +8,42 @@ public class Account {
 
     private static final String ACCOUNT_NUMBER_FILE_NAME = "number.txt";
 
+    private int number;
     private String forename;
     private String surname;
-    private Bank bank;
-    private int number;
     private double balance;
+    private String password;
 
-    public Account(String forename, String surname) {
+    public Account(String forename, String surname, String password, int balance) {
+        this.number = getNextNumber();
         this.forename = forename;
         this.surname = surname;
-        this.number = getNextNumber();
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public Account(String forename, String surname, String password) {
+        this(forename, surname, password, 0);
+    }
+
+    // Constructor from data stored in file
+    public Account(String fileData) {
+        String[] data = fileData.split(",");
+        number = Integer.parseInt(data[0]);
+        forename = data[1];
+        surname = data[2];
+        balance = Double.parseDouble(data[3]);
+        password = data[4];
+    }
+
+    // Returns the details of the account as a CSV String in the form:
+    // number,forename,surname,balance
+    public String getAsCSVString() {
+        return number + "," + forename + "," + surname + "," + balance + "," + password;
+    }
+
+    public String toString() {
+        return String.format("%d: %s %s, £%.2f, %s", number, forename, surname, balance, password);
     }
 
     public String getForename() {
@@ -38,6 +64,15 @@ public class Account {
 
     public String getFullName() {
         return forename + " " + surname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // More secure than just a getter
+    public boolean isPassword(String password) {
+        return password.equals(this.password);
     }
 
     public double getBalance() {
@@ -70,7 +105,7 @@ public class Account {
     // Reads the account number from the file and increments it. Returns -1 for an
     // error
     private int getNextNumber() {
-        int num;
+        int num = -1;
 
         // Reads the file, creating a new one if no such file exists
         try {
@@ -87,19 +122,17 @@ public class Account {
             PrintWriter writer = new PrintWriter(ACCOUNT_NUMBER_FILE_NAME);
             writer.print(num + 1);
             writer.close();
-
-            return num;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return -1;
+        return num;
     }
 
     public static void main(String[] args) {
-        Account account1 = new Account("Tom", "Fardell");
-        Account account2 = new Account("Matthew", "Hilton");
+        Account account1 = new Account("Tom", "Fardell", "gaypenis");
+        Account account2 = new Account("Matthew", "Hilton", "sexwithmen");
         System.out.println(account1.number);
         System.out.println(account2.number);
     }
--- Bank.java
+++ Bank.java
@@ -1,20 +1,120 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Bank {
 
-    static final int MAX_ACCOUNTS = 1000;
+    private ArrayList<Account> accounts;
+    private String accountFile;
 
-    private Account[] accounts;
-
-    public Bank() {
-        accounts = new Account[MAX_ACCOUNTS];
+    public Bank(String accountFile) {
+        this.accountFile = accountFile;
+        accounts = getAccountsFromFile();
     }
 
     public Account getAccount(int number) {
-        for (int i = 0; i < MAX_ACCOUNTS; i++) {
-            if (number == accounts[i].getNumber()) {
-                return accounts[i];
+        for (int i = 0; i < accounts.size(); i++) {
+            if (number == accounts.get(i).getNumber()) {
+                return accounts.get(i);
             }
         }
 
         return null;
+    }
+
+    // Gets the number of accounts stored in the file. Returns -1 for error
+    private int getAccountFileLength() {
+        int i = 0;
+
+        try {
+            File file = new File(accountFile);
+            if (file.createNewFile()) {
+                return 0;
+            } else {
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+                    scanner.nextLine();
+                    i++;
+                }
+
+                scanner.close();
+                return i;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    // Reads the account file and returns an array of accounts
+    private ArrayList<Account> getAccountsFromFile() {
+        ArrayList<Account> accounts = new ArrayList<Account>();
+
+        try {
+            File file = new File(accountFile);
+
+            if (!file.createNewFile()) {
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+                    accounts.add(new Account(scanner.nextLine()));
+                }
+
+                scanner.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return accounts;
+    }
+
+    // Writes the accounts array to the file
+    public void writeAccountsToFile() {
+        try {
+            PrintWriter writer = new PrintWriter(accountFile);
+
+            for (int i = 0; i < accounts.size(); i++) {
+                writer.println(accounts.get(i).getAsCSVString());
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Adds an account to the bank
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public String toString() {
+        String result = "";
+        result += String.format("%d accounts:", accounts.size());
+
+        for (int i = 0; i < accounts.size(); i++) {
+            result += "\n" + accounts.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Bank bank = new Bank("accounts.txt");
+        System.out.println(bank);
+
+        bank.getAccount(14).depositFunds(4284.68);
+        bank.getAccount(15).depositFunds(20.41);
+        bank.getAccount(15).withdrawFunds(400);
+        bank.getAccount(15).withdrawFunds(12);
+
+        System.out.println(bank);
     }
 }
